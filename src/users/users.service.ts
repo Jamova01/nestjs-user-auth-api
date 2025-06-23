fc9f953b3dba172a99eb8a4a4f0cac5c2726ca47
Jamova01 <jorgemova01@gmail.com>
2025-06-23 00:03:48 -0500

    feat: hash user passwords on create and update in UsersService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,14 +3,22 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/database/prisma.service';
 import { User } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
-  create(createUserDto: CreateUserDto): Promise<User> {
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const { password, ...rest } = createUserDto;
+    const saltRounds = 10;
+    const hash = await bcrypt.hash(password, saltRounds);
+
     return this.prisma.user.create({
-      data: createUserDto,
+      data: {
+        ...rest,
+        password: hash,
+      },
     });
   }
 
@@ -28,10 +36,23 @@ export class UsersService {
 
   async update(id: string, updateUserDto: UpdateUserDto): Promise<User> {
     await this.findOne(id);
-    return this.prisma.user.update({
+
+    const { password, ...rest } = updateUserDto;
+
+    const data: Partial<User> = { ...rest };
+
+    if (password) {
+      const saltRounds = 10;
+      const hashedPassword = await bcrypt.hash(password, saltRounds);
+      data.password = hashedPassword;
+    }
+
+    const updatedUser = await this.prisma.user.update({
       where: { id },
-      data: updateUserDto,
+      data,
     });
+
+    return updatedUser;
   }
 
   async remove(id: string) {
