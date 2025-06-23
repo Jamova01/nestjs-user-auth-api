7cb522cd133b7292403ae1c4a54f1648c12e1cdf
Jamova01 <jorgemova01@gmail.com>
2025-06-22 19:40:36 -0500

    chore: add initial config files for database, JWT, and pgAdmin

--- src/config/jwt.config.ts
+++ src/config/jwt.config.ts
@@ -2,5 +2,5 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('jwt', () => ({
   secret: process.env.JWT_SECRET,
-  expiresIn: process.env.JWT_EXPIRES_IN || '1h', // valor por defecto opcional
+  expiresIn: process.env.JWT_EXPIRES_IN || '1h',
 }));
