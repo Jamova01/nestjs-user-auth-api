237d8a47b6e641e1b598d2bd623f43d2e8fa5b12
Jamova01 <jorgemova01@gmail.com>
2025-06-23 11:35:19 -0500

    chore: update Swagger config with API details, tags, and JWT bearer auth

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,21 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const swaggerConfig = new DocumentBuilder()
-    .setTitle('Cats API')
-    .setDescription('API for managing cats')
-    .setVersion('1.0')
-    .addTag('cats')
+    .setTitle('NestJS Auth API – User Management & JWT')
+    .setDescription(
+      'RESTful API built with NestJS, Prisma, and PostgreSQL for secure user authentication, registration, login, role-based access control (RBAC), and token refresh using JWT.',
+    )
+    .setVersion('1.0.0')
+    .addTag('Auth', 'Endpoints related to authentication')
+    .addTag('Users', 'Endpoints for managing user data')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+      'JWT-auth',
+    )
     .build();
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
