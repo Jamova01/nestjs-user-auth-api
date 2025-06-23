3d1cf78db6340a13a671fc6164b4db5a4710aadf
Jamova01 <jorgemova01@gmail.com>
2025-06-22 23:10:36 -0500

    feat: add CreateUserDto with validation decorators

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,30 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
+import { Role } from '@prisma/client';
+
+export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsOptional()
+  @IsEnum(Role, {
+    message: `Role must be one of: ${Object.values(Role).join(', ')}`,
+  })
+  role?: Role;
+}
