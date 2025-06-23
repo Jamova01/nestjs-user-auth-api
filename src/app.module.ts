2e1782890281b91e420da6035630db9d8cf49472
Jamova01 <jorgemova01@gmail.com>
2025-06-22 22:35:58 -0500

    chore: import PrismaModule into AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { PrismaModule } from './database/prisma.module';
 import databaseConfig from './config/database.config';
 import pgadminConfig from './config/pgadmin.config';
 import jwtConfig from './config/jwt.config';
@@ -16,6 +17,7 @@ import jwtConfig from './config/jwt.config';
     }),
     AuthModule,
     UsersModule,
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
