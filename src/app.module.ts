a4b8d0e7ed4e1397a245e7a3befcedcafe3d70fa
Jamova01 <jorgemova01@gmail.com>
2025-06-22 21:18:54 -0500

    chore: import AuthModule and UsersModule into AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 import databaseConfig from './config/database.config';
 import pgadminConfig from './config/pgadmin.config';
 import jwtConfig from './config/jwt.config';
@@ -12,6 +14,8 @@ import jwtConfig from './config/jwt.config';
       isGlobal: true,
       load: [databaseConfig, pgadminConfig, jwtConfig],
     }),
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
