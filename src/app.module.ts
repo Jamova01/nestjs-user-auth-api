6209d60c624e3678e95f9c1a3c2ff2af327d71b5
Jamova01 <jorgemova01@gmail.com>
2025-06-22 19:39:54 -0500

    chore: load database, pgAdmin, and JWT configs in ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import databaseConfig from './config/database.config';
+import pgadminConfig from './config/pgadmin.config';
+import jwtConfig from './config/jwt.config';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig, pgadminConfig, jwtConfig],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
