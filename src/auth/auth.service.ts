64ff525e82456fc74d3fdff86449ec59f960c101
Jamova01 <jorgemova01@gmail.com>
2025-06-24 11:47:56 -0500

    feat: implement user registration in AuthService with password hashing and default role

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-
 import * as bcrypt from 'bcrypt';
 import { plainToInstance } from 'class-transformer';
+
+import { User, Role } from '@prisma/client';
 
 import { UsersService } from 'src/users/users.service';
 import { UserResponseDto } from 'src/users/dto/user-response.dto';
 
 import { AuthResponseDto } from './dto/auth-response.dto';
 import { AuthTokenDto } from './dto/auth-token.dto';
+import { RegisterDto } from './dto/register.dto';
+
+import { SALT_ROUNDS } from 'src/common/constants';
 
 @Injectable()
 export class AuthService {
@@ -34,10 +38,25 @@ export class AuthService {
     });
   }
 
-  login(user: { email: string; id: string }): AuthTokenDto {
+  login(user: Pick<User, 'email' | 'id'>): AuthTokenDto {
     const payload = { email: user.email, sub: user.id };
     return {
       access_token: this.jwtService.sign(payload),
     };
+  }
+
+  async register(registerDto: RegisterDto): Promise<UserResponseDto> {
+    const { password, ...rest } = registerDto;
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+
+    const user = await this.usersService.create({
+      ...rest,
+      password: hashedPassword,
+      role: Role.USER,
+    });
+
+    return plainToInstance(UserResponseDto, user, {
+      excludeExtraneousValues: true,
+    });
   }
 }
