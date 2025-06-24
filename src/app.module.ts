09da77447026279a785ffd9387aed46ffbae9b12
Jamova01 <jorgemova01@gmail.com>
2025-06-24 09:49:45 -0500

    chore: register JwtAuthGuard as global guard in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+
 import { PrismaModule } from './database/prisma.module';
+
+import { AuthModule } from './auth/auth.module';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+
+import { UsersModule } from './users/users.module';
+
 import databaseConfig from './config/database.config';
 import pgadminConfig from './config/pgadmin.config';
 import jwtConfig from './config/jwt.config';
@@ -15,11 +22,17 @@ import jwtConfig from './config/jwt.config';
       isGlobal: true,
       load: [databaseConfig, pgadminConfig, jwtConfig],
     }),
+    PrismaModule,
     AuthModule,
     UsersModule,
-    PrismaModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
