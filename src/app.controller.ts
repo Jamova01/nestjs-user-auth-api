4980848eebccda840f4110b65767a8856cc20343
Jamova01 <jorgemova01@gmail.com>
2025-06-24 09:51:44 -0500

    feat: add login and profile endpoints with auth guards and Swagger docs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,36 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
+
+import { AuthService } from './auth/auth.service';
+
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+
+import { Public } from './auth/decorators/public.decorator';
+
+import { AuthTokenDto } from './auth/dto/auth-token.dto';
+import { RequestUser } from './auth/interfaces/user-from-request.interface';
+
+import { ApiOkResponse, ApiUnauthorizedResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private authService: AuthService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Public()
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  @ApiOkResponse({
+    description: 'Login successful.',
+    type: AuthTokenDto,
+  })
+  @ApiUnauthorizedResponse({ description: 'Invalid credentials.' })
+  login(@Request() req: { user: RequestUser }): AuthTokenDto {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req: { user: RequestUser }): RequestUser {
+    return req.user;
   }
 }
