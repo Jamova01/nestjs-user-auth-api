d132eadf45b1b3ee80388eee89214e35a44303e2
Jamova01 <jorgemova01@gmail.com>
2025-06-27 13:52:18 -0500

    feat(auth): add Swagger docs and tag to AuthController endpoints

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,92 @@
+import {
+  Controller,
+  Request,
+  Post,
+  UseGuards,
+  Get,
+  Body,
+} from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+
+import { Public } from 'src/common/decorators/public.decorator';
+import { RequestUser } from './interfaces/user-from-request.interface';
+
+import { AuthTokenDto } from './dto/auth-token.dto';
+import { RegisterDto } from './dto/register.dto';
+import { UserResponseDto } from 'src/users/dto/user-response.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Public()
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  @ApiOkResponse({
+    description: 'Login successful.',
+    type: AuthTokenDto,
+  })
+  @ApiUnauthorizedResponse({ description: 'Invalid credentials.' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        email: { type: 'string', example: 'user@example.com' },
+        password: { type: 'string', example: 'strongPassword123' },
+      },
+    },
+  })
+  login(@Request() req: { user: RequestUser }): AuthTokenDto {
+    return this.authService.login(req.user);
+  }
+
+  @Public()
+  @Post('register')
+  @ApiCreatedResponse({
+    type: UserResponseDto,
+    description: 'User created successfully',
+  })
+  @ApiBadRequestResponse({
+    description: 'Validation failed or email already exists',
+  })
+  @ApiBody({
+    type: RegisterDto,
+    description: 'Create a new user with email, password and optional role',
+    examples: {
+      user: {
+        summary: 'Create standard user (role optional)',
+        value: {
+          name: 'Andrés Perez',
+          email: 'andres@example.com',
+          password: 'strongPassword123',
+        },
+      },
+    },
+  })
+  register(@Body() registerDto: RegisterDto): Promise<UserResponseDto> {
+    return this.authService.register(registerDto);
+  }
+
+  @ApiBearerAuth('JWT-auth')
+  @Get('profile')
+  @ApiOkResponse({
+    description: 'Returns the authenticated user or admin information',
+    type: UserResponseDto,
+  })
+  @ApiUnauthorizedResponse({
+    description: 'Unauthorized: missing or invalid JWT token',
+  })
+  getProfile(@Request() req: { user: RequestUser }): RequestUser {
+    return req.user;
+  }
+}
