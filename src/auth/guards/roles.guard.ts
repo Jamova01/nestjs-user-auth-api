f3fc3e9bf57474cd53ae6db583a9a4b4f9553b45
Jamova01 <jorgemova01@gmail.com>
2025-06-27 12:37:07 -0500

    feat(auth): add Roles decorator for role-based access control

---
+++ src/auth/guards/roles.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Role } from '@prisma/client';
+import { ROLES_KEY } from 'src/common/decorators/roles.decorator';
+import { Request } from 'express';
+
+interface JwtPayloadWithRole {
+  id: string;
+  email: string;
+  role: Role;
+}
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest<Request>();
+    const user = request.user as JwtPayloadWithRole;
+
+    console.log('🔍 USER OBJECT:', JSON.stringify(user, null, 2));
+
+    return requiredRoles.includes(user.role);
+  }
+}
