d36b6204e669b9c94ad5d6f8690ce7aa7749eb3b
Jamova01 <jorgemova01@gmail.com>
2025-06-28 18:23:26 -0500

    feat(profile): add /profile/me endpoint to return authenticated user

--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { ProfileController } from './profile.controller';
-import { PrismaService } from 'src/database/prisma.service';
 
 @Module({
   providers: [ProfileService],
   controllers: [ProfileController],
 })
-export class ProfileModule {
-  constructor(private prisma: PrismaService) {}
-}
+export class ProfileModule {}
